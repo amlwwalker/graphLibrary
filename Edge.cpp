d17fda8e88936acff7d30a2c790d48736e1f4f9b
Alex <alex@decoded.co>
2014-10-31 11:35:26 +0000

    more edge stuff

--- Edge.cpp
+++ Edge.cpp
@@ -1,17 +1,1 @@
 
-#include "Edge.hpp"
-#include "Node.hpp"
-#include <vector>
-#include <string.h>
-
-class Edge {
-
-public:
-	// Node getEnd1(){return end1;}
-	// Node getEnd2(){return end2;}
-private:
-	vector<string>	types;
-	vector<string>		properties;
-	// Node end1;
-	// Node end2;
-}
---
+++ Edge.hpp
@@ -0,0 +1,18 @@
+// #include "Node.hpp"
+#include <vector>
+#include <map>
+#include <string.h>
+
+using namespace std;
+
+class Edge {
+
+public:
+	// Node getEnd1(){return end1;}
+	// Node getEnd2(){return end2;}
+private:
+	// vector<string>	types;
+	map<string, string>		properties;
+	// Node end1;
+	// Node end2;
+};
