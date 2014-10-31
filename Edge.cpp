d5f6361133b56fb2d254a0d343c7148d9498b7d4
Alex <alex@decoded.co>
2014-10-31 11:18:00 +0000

    adding edges

---
+++ Edge.cpp
@@ -0,0 +1,17 @@
+
+#include "Edge.hpp"
+#include "Node.hpp"
+#include <vector>
+#include <string.h>
+
+class Edge {
+
+public:
+	// Node getEnd1(){return end1;}
+	// Node getEnd2(){return end2;}
+private:
+	vector<string>	types;
+	vector<string>		properties;
+	// Node end1;
+	// Node end2;
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+// 'Hello World!' program 
+ 
+#include <iostream>
+#include "Edge.hpp"
+using namespace std;
+
+int main()
+{
+
+
+  cout << "Denise and Alex's insane graph database" << endl;
+  return 0;
+}
