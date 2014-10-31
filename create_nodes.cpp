723f65e4542068fc1e5faa6b607bb1840b6bfc7c
Denise Xifara <denise@decoded.co>
2014-10-31 11:17:26 +0000

    adding node class

---
+++ create_nodes.cpp
@@ -0,0 +1,37 @@
+
+
+
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <vector>
+#include <list>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <algorithm> 
+#include <iterator>
+#include <string.h>
+#include <stdio.h>
+#include <numeric>
+
+using namespace std;
+
+
+class Node {
+
+public:
+
+
+private:
+	vector<string> Labels;
+	vector <vector <string> > Properties;
+
+};
+
+
+int main () {
+
+
+}
