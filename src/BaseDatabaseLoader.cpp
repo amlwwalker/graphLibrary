7c3b1557d01b9ce0c2a8e3a82138302ae86983c2
Alex <alex@decoded.co>
2014-12-17 18:03:47 +0000

    adding base database loader

---
+++ src/BaseDatabaseLoader.cpp
@@ -0,0 +1,13 @@
+// # 2014 Walking Software
+
+#include "BaseDatabaseLoader.hpp"
+#include <iomanip>      // std::setprecision
+
+// BaseDatabaseLoader::BaseDatabaseLoader(std::string aFileName, BaseGraph &graph) 
+// 										: mFileName(aFileName), mGraph(graph) {
+// 	//constructor
+// }
+// BaseDatabaseLoader::~BaseDatabaseLoader(){
+// //destructor
+// }
+
---
+++ src/BaseDatabaseLoader.hpp
@@ -0,0 +1,27 @@
+// # 2014 Walking Software
+#ifndef __BASE_DATABASELOADER_HPP_
+#define __BASE_DATABASELOADER_HPP_
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include "GraphObjects/BaseGraph.hpp"
+#include "../../rapidjson/document.h"		// rapidjson's DOM-style API
+#include "../../rapidjson/prettywriter.h"	// for stringify JSON
+#include "../../rapidjson/filestream.h"	// wrapper of C stream for prettywriter as output
+
+namespace graphDB {
+
+	class BaseDatabaseLoader {
+
+		public:
+			BaseDatabaseLoader(std::string aFileName, BaseGraph &graph);
+			// ~BaseDatabaseLoader();
+			void loadDatabase();
+		protected:
+			
+			std::string mFileName;
+			BaseGraph &mGraph;
+	};
+}
+
+#endif
